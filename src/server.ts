f1e25f13396dbb1cf813d824508072555dbe127d
Minhaz <minhazulabedin44@gmail.com>
2023-06-02 04:53:10 +0600

    testing

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("data base connect successful");
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (error: any) {
+    console.log("error heppend", error);
+  }
+}
+main();
