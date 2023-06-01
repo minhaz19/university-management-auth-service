b4590b7da27d2f47bfb769a72be212761e6fd59b
Minhaz <minhazulabedin44@gmail.com>
2023-06-02 05:12:25 +0600

    testing lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express, { Application } from "express";
-import cors from "cors";
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-                        app.use(cors())
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-export default app;
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
+})
+
+export default app
